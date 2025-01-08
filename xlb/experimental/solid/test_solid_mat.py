a58d6e297fae6c8f632e06b91e74d69de718df3c
Lukas Bürgler <buelukas@ethz.ch>
2025-01-08 16:11:59 +0100

    Added functionals for the computation phi_x and phi_y

--- xlb/experimental/solid/test_solid_mat.py
+++ xlb/experimental/solid/test_solid_mat.py
@@ -24,10 +24,17 @@ if __name__ == "__main__":
 
     velocity_set = xlb.velocity_set.D2Q4(precision_policy=precision_policy, backend=backend)
     omega = 1.0
+    delta_x = 1.0
+    delta_t = 1.0
+    c = delta_x/delta_t
+    c_k = 1.0
+    c_m = 1.0
+
     grid = grid_factory(grid_shape, compute_backend=backend)
     vector_size = 5
     cardinality = vector_size * velocity_set.q
     f = grid.create_field(cardinality=cardinality, dtype=precision_policy.store_precision)
+    feq = grid.create_field(cardinality=cardinality, dtype=precision_policy.store_precision)
     U_num = grid.create_field(cardinality=vector_size, dtype=precision_policy.store_precision)
     B = grid.create_field(cardinality=vector_size, dtype=precision_policy.store_precision)
     _vector_vec = wp.vec(vector_size, dtype=precision_policy.compute_precision.wp_dtype)
@@ -51,13 +58,43 @@ if __name__ == "__main__":
     def write_U_num(U_num: Any, index: Any, _U: Any):
         for j in range(vector_size):
             U_num[j, index[0], index[1], index[2]] = precision_policy.store_precision.wp_dtype(_U[j])
+    
+    @wp.func
+    def phi_x(_U: Any, c_k: Any, c_m: Any):
+        phi_x = _vector_vec()
+        phi_x[0] = c_k*_U[2]+c_m*_U[3]
+        phi_x[1] = c_m*_U[4]
+        phi_x[2] = c_k*_U[0]
+        phi_x[3] = c_m*_U[0]
+        phi_x[4] = c_m*_U[1]
+        return phi_x
+    
+    @wp.func
+    def phi_y(_U: Any, c_k: Any, c_m: Any):
+        phi_y = _vector_vec()
+        phi_y[0] = c_m*_U[4]
+        phi_y[1] = c_k*_U[2]-c_m*_U[3]
+        phi_y[2] = c_k*_U[1]
+        phi_y[3] = -c_m*_U[1]
+        phi_y[4] = c_m*_U[0]
+        return phi_y
+    
+    @wp.func
+    def _feq(_U: Any, phi_x: Any, phi_y: Any):
+        _feq = _vector_mat()
+        for i in range(vector_size):
+            for j in range(velocity_set.q):
+
+                _feq[i,j]=(_U[i]+2.0/c*(velocity_set._c_float[0,j]*phi_x[i]+velocity_set._c_float[1,j]*phi_y[i]))/4.0
+
+
+        return _feq
 
     @wp.kernel
-    def init_kernel(f: wp.array4d(dtype=Any), U_num: wp.array4d(dtype=Any)):
+    def init_kernel(f: wp.array4d(dtype=Any), U_num: wp.array4d(dtype=Any), B: wp.array4d(dtype=Any), feq: wp.array4d(dtype=Any)):
         i, j, k = wp.tid()
         index = wp.vec3i(i, j, k)
         _f = read_pop_functional(f, index)
-        
         for i in range(vector_size):
             for j in range(velocity_set.q):
                 _f[i, j] = precision_policy.compute_precision.wp_dtype(1.0)
@@ -67,8 +104,17 @@ if __name__ == "__main__":
         for i in range(vector_size):
             for j in range(velocity_set.q):
                 _U[i] = _U[i] + _f[i,j]
+            _U[i] = _U[i] + B[i, index[0], index[1], index[2]]*delta_t/2.0
         write_U_num(U_num, index, _U)
 
-wp.launch(init_kernel, inputs=[f,U_num], dim=f.shape[1:])
+        phi_x = phi_x(_U, c_k, c_m)
+        phi_y = phi_y(_U, c_k, c_m)
+
+        
+        _feq = _feq(_U, phi_x, phi_y)
+        write_pop_functional(feq, index, _feq)
+
+wp.launch(init_kernel, inputs=[f,U_num,B,feq], dim=f.shape[1:])
 print(f.numpy())
 print(U_num.numpy())
+print(feq.numpy())
